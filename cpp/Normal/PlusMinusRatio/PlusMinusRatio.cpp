b125feb4828b81e068a636ef3f44f5274335587f
LeMeTwo <teamgoohsmod@gmail.com>
2023-06-02 20:06:52 +0200

    Two daily tasks about vectors

---
+++ cpp/Normal/PlusMinusRatio/PlusMinusRatio.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+
+/*
+Function Description
+
+Complete the plusMinus function in the editor below.
+
+plusMinus has the following parameter(s):
+
+    int arr[n]: an array of integers
+
+Print
+Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with
+
+digits after the decimal. The function should not return a value.
+
+Input Format
+
+The first line contains an integer,
+, the size of the array.
+The second line contains space-separated integers that describe .
+*/
+
+void plusMinus(vector<int> arr) {
+    double plus = 0;
+    double zero = 0;
+    double minus = 0;
+    int n = arr.size();
+    
+    for(int i = 0; i < n; i++){
+        if(arr[i] == 0){
+            zero += 1;
+        }
+        else if(arr[i] > 0){
+            plus += 1;
+        }
+        else if(arr[i] < 0){
+            minus += 1;
+        }
+    }
+
+    printf("%0.6lf\n%0.6lf\n%0.6lf\n", plus/n, minus/n, zero/n);
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
